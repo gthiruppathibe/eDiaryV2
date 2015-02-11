fad4398b506e83ed22b6a0faf2bfa5f05994f6b9
gthiruppathibe <thiruppathi.gandhi@mutualmobile.com>
2015-02-11 22:07:57 +0530

    changes

--- eDiary/Login/EDRLoginInteractor.m
+++ eDiary/Login/EDRLoginInteractor.m
@@ -19,30 +19,14 @@
 
 - (void) loginCredentialWithEmail:(NSString*)email Password:(NSString*)password{
  
-    //perform async login request
-    dispatch_block_t loginRequestBlock = ^{ @autoreleasepool {
-        
-        [self processLoginRequestWithEmail:email Password:password];
-        
-    }};
-    dispatch_async(dispatch_get_main_queue(), loginRequestBlock);
+    dispatch_async (dispatch_get_main_queue(), ^{
+        //send a request to web service or core data
+        //receive response from web service or core data and send to presener
+         [self processLoginResponse:0]; //1- success,0-failure
+    });
 }
 
 #pragma mark process the message
-
-- (void) processLoginRequestWithEmail:(NSString*)email Password:(NSString*)password {
-
-    //TODO: send a request to core data or web service to validate the account details
-    //TODO: Assume that i send async request to web service or core data
-    
-    //TODO: Assume i got async response from server
-    dispatch_block_t loginResponseBlock = ^{ @autoreleasepool {
-        
-        [self processLoginResponse:0]; //1- success,0-failure
-        
-    }};
-    dispatch_async(dispatch_get_main_queue(), loginResponseBlock);
-}
 
 - (void) processLoginResponse:(NSInteger) authentication {
     
